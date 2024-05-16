46ccf93e01cdce6019bb32edc406edc920b81b35
Matthew Vernacchia <mvernacc@mit.edu>
2024-05-15 23:11:37 -0400

    Example that does not work because of downcast on Box.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use svg::node::element::{Circle, Group};
+
 fn main() {
-    println!("Hello, world!");
+    let group = Group::new()
+        .add(Circle::new().set("cx", 1.0))
+        .add(Circle::new().set("cx", 2.0));
+
+    for node in group.get_children() {
+        // I want to print the cx attribute of each child node which is a Circle.
+
+        if let Some(c) = node.downcast_ref::<Circle>() {
+            println!("Its a circle.");
+            let attrs = c.get_attributes();
+            println!("{:?}", attrs.get("cx"));
+        }
+    }
 }
